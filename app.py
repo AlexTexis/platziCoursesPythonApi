71910c2a4911321acfc3faa6e9a5a73bc1d2c2c3
alexTexis <alextexis@hotmail.com>
2019-08-09 14:39:22 -0500

    start 2

--- app.py
+++ app.py
@@ -1,50 +1,20 @@
 from flask import Flask, jsonify, request
-from bson.json_util import dumps, ObjectId
-import json
-from flask_pymongo import PyMongo
+from db import mongo
 from flask_cors import CORS
-
-# import os
-
-# heroku_mdb = os.environ.get('MDB', None)
+from groups import groups_bp
 
 app = Flask(__name__)
 config_object = 'settings'
 app.config.from_object(config_object)
-print(app.config)
 CORS(app)
-
-# if heroku_mdb:
-#     app.config['MONGO_URI'] = heroku_mdb
-# else:
-#     app.config['MONGO_URI'] = 'mongodb://localhost/mycontacts_db'
-
-mongo = PyMongo(app)
-
-# Routes and Controllers
-
-# GET /
-# @app.route('/', methods=['GET'])
-# def index():
-#     return jsonify({
-#         'msg': 'API MYCONTACTS PY',
-#         'status': 'ok'
-#     })
-
+app.register_blueprint(groups_bp)
+mongo.init_app(app)
 
 
 @app.route('/',methods=['GET'])
 def groups():
     return jsonify({
-    'groups' : 'json.loads(res)'
-    })
-
-@app.route('/groups',methods=['GET'])
-def groups_get():
-    collection = mongo.db.groups
-    res = dumps( collection.find({}) )
-    return jsonify({
-    'groups' : json.loads(res)
+    'message' : 'hello'
     })
 
 
